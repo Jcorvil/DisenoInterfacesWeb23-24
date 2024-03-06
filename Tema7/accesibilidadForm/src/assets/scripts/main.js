a5605fa4a3acae107b94d379659aadd9af124e35
unknown <jcorvil600@g.educaand.es>
2024-03-06 13:14:21 +0100

    05/03/2024

--- Tema7/accesibilidadForm/src/assets/scripts/main.js
+++ Tema7/accesibilidadForm/src/assets/scripts/main.js
@@ -1,4 +1,5 @@
 function onIniciar() {
+  // ---------------CÓDIGO TABS--------------- //
   // 1.- Coneguimos todas las pestañas(botones)
   const botonesTab = document.querySelectorAll(".botonesTab__pestana");
   // 2.- Conseguimos los bloques de texto
@@ -19,6 +20,27 @@ function onIniciar() {
 
       boton.classList.add("activo");
       bloquesTexto[i].classList.add("activo");
+    });
+  });
+
+  // ---------------CÓDIGO ACORDEÓN--------------- //
+  // h2Titulo va a tener un eventListener de click
+  const h2Titulo = document.querySelectorAll(".divContenedor__titulo");
+  // pContenido es al que vamos a modificarle las clases css
+  const pContenido = document.querySelectorAll(".divContenedor__contenido");
+
+  console.log(h2Titulo);
+  console.log(pContenido);
+
+  h2Titulo.forEach((nodo, i) => {
+    nodo.addEventListener("click", () => {
+      if (
+        pContenido[i].classList.contains("divContenedor__contenido--activo")
+      ) {
+        pContenido[i].classList.remove("divContenedor__contenido--activo");
+      } else {
+        pContenido[i].classList.add("divContenedor__contenido--activo");
+      }
     });
   });
 }
